368e5b7cdaef26d0fde63d9e88a1b5f18f0152fb
faragh47 <faragh@mohammadrezas-MacBook-Pro.local>
2023-01-22 00:14:46 +0330

    fist commit from back

--- src/apis.js
+++ src/apis.js
@@ -3,10 +3,24 @@ export default function Apis({getState,token,getDateAndTime,showAlert,AIOService
     return {
         sabte_shomare_tamas(shomare_tamas){
             debugger;
+
+    
             //نمونه درخواست get
             //let res = await Axios.get(url);
             //نمونه درخواست post
-            //let res = await Axios.post(url,body);
+            let res = await Axios.post(url,{
+                "firstName": "مشتریان",
+                "lastName": "مشتریان",
+                "fatherName": "مشتریان",
+                "nationalId": "۱۱۱۱۱۱۱۱۱۱",
+                "genderTypeId": "۱",
+                "mobileNumbers": [
+                  {
+                    "mobileNumber":shomare_tamas,
+                    "isDefault": true,
+                  }
+                ]
+              });
         }
     }
 }
