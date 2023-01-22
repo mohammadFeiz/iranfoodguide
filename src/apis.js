81ba68cef666abeca537a029d228b02c3f1fd42b
faraghdani.m <faraghdani.m@ICT-PC-28>
2023-01-22 13:43:21 +0330

    edit in apis

--- src/apis.js
+++ src/apis.js
@@ -7,16 +7,19 @@ export default function Apis({getState,token,getDateAndTime,showAlert,AIOService
             //نمونه درخواست get
             //let res = await Axios.get(url);
             //نمونه درخواست post
-            let res = await Axios.post(url,{
+            let url='https://localhost:7203/api/People/Create';
+            
+            let res = await Axios.post(url,
+              {
                 "firstName": "مشتریان",
                 "lastName": "مشتریان",
                 "fatherName": "مشتریان",
                 "nationalId": "۱۱۱۱۱۱۱۱۱۱",
-                "genderTypeId": "۱",
+                "genderTypeId": 1,
                 "mobileNumbers": [
                   {
-                    "mobileNumber":shomare_tamas,
-                    "isDefault": true,
+                    "mobileNumber": shomare_tamas,
+                    "isDefault": true
                   }
                 ]
               });
