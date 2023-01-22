4c1c87c07247d194631f3d0360f728583928c310
MFeiz <30654631+mohammadFeiz@users.noreply.github.com>
2023-01-22 21:19:19 +0330

    Update apis.js

--- src/apis.js
+++ src/apis.js
@@ -2,7 +2,6 @@ import Axios from 'axios';
 export default function Apis({getState,token,getDateAndTime,showAlert,AIOServiceShowAlert,baseUrl}){
     return {
         async sabte_shomare_tamas(shomare_tamas){
-
     
             //نمونه درخواست get
             //let res = await Axios.get(url);
@@ -15,8 +14,32 @@ export default function Apis({getState,token,getDateAndTime,showAlert,AIOService
                     "MobileNumber": shomare_tamas,
                     "IsDefault": true
               });
-              if(res.data.isSuccess){return true}
-              else {return res.data.message}
+              try{
+                if(res.data.isSuccess){
+                    AIOServiceShowAlert({
+                        type:'success',
+                        text:'شماره شما با موفقیت ثبت شد',
+                        subtext:res.data.message
+                    })
+                    return true
+                }
+                else {
+                    AIOServiceShowAlert({
+                        type:'error',
+                        text:'شماره ثبت نشد',
+                        subtext:res.data.message
+                    })
+                    return false
+                }
+              }
+              catch(error){
+                AIOServiceShowAlert({
+                    type:'error',
+                    text:'شماره ثبت نشد',
+                    subtext:error.message
+                })
+                return false
+              }
         }
     }
 }
