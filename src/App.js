9131c26f25b6a0fdbc66c1051915b537735846d4
mohammadreza farghadani <mohmmad_kratos47@yahoo.com>
2023-08-20 17:08:16 +0330

    edit in apijs

--- src/App.js
+++ src/App.js
@@ -23,8 +23,8 @@ import './App.css';
 export default class App extends Component {
   constructor(props) {
     super(props);
-    this.baseUrl = 'https://localhost:7203'
-   //this.baseUrl = 'https://iranfoodguide.ir'
+    //this.baseUrl = 'https://localhost:7203'
+   this.baseUrl = 'https://iranfoodguide.ir'
     this.state = {
       isLogin: false,
       isRegistered:false,
